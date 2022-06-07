7eeaea71faa72d8d0282bd1f55984a5f169d7366
rodolfocerutti <42013617+rodolfocerutti@users.noreply.github.com>
2022-06-06 21:09:29 -0300

    primeiro codigo

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const span = document.querySelector('span')
+const btnStart = document.querySelector('#btnStart')
+const btnPause = document.querySelector('#btnPause')
+const btnClean = document.querySelector('#btnClean')
+
+let interval
+
+btnStart.onclick = () => {
+    btnStart.disabled = true
+    btnPause.disabled = false
+
+    let value = parseFloat(span.innerHTML)
+
+    interval = setInterval(() => {
+        valeu += 0.1
+        
+        span.innerHTML = value.toFixed(1)
+    }, 100);
+}
+
+btnPause.onclick = () => {
+    btnStart.disabled = false
+    btnPause.disabled = true
+  
+    clearInterval(interval)
+  }
+  
+  btnClean.onclick = () => {
+    btnStart.disabled = false
+    btnPause.disabled = true
+    
+    clearInterval(interval)
+    span.innerHTML = "0.0"
+  }
